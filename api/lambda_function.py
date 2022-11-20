4020c0c45954747e236cdee4d10332e9320f7692
Sagar Bansal <sagbansa@publicisgroupe.net>
2022-11-21 02:54:43 +0530

    API

--- api/lambda_function.py
+++ api/lambda_function.py
@@ -6,13 +6,41 @@ adx = boto3.client('dataexchange', region_name='us-east-1')
 
 def lambda_handler(event,context):
     
-    body =  "{\n  ursa_udp_availablemetadatarecord(\n limit: 10\n sortBy: {\n      field: \"content.catalogProperties.startTimeUTC\",\n      sortOrder: \"ASC\"\n    }\n     content: { catalogProperties: { vendor: { eq: \"ICEYE\" } passType:{eq: \"DESCENDING\"} antennaPointing:{eq: \"RIGHT\"} acquisitionMode:{eq:\"ICE_SPOTLIGHT\"} } }\n  ) {\n    assignedId\n    uploadTime\n    header {\n      version\n      commonProperties {\n        sourceVendor\n        startTimeUTC\n        endTimeUTC\n      }\n    }\n    content {\n      version\n      catalogProperties {\n        quicklookUrl\n        wktPolygon\n        geoJsonPolygon {\n          type\n          coordinates\n        }\n        vendor\n        startTimeUTC\n        endTimeUTC\n        passType\n        acquisitionMode\n        incidence {\n          incidenceNearDegs\n          incidenceFarDegs\n        }\n        collectionPlatform\n        antennaPointing\n      }\n    }\n  }\n}\n"
-
-
+    startdate = "2021-01-01T00:00:00Z"
+    enddate = "2022-11-01T00:00:00Z"
+    intersectsWKT = ""
+    if "startdate" in event and event["startdate"] != "":
+        startdate = event["startdate"]
+    
+    if "enddate" in event and event["enddate"] != "":
+        enddate = event["enddate"]
+        
+    if "area" in event and event["area"] != "":
+        area = event["area"]
+        intersectsWKT = "intersectsWKT: \""+ area + "\""
+    
+    body =  "{ursa_udp_availablemetadatarecord( \
+        header:{ commonProperties:{imageBoundsGeoJsonPolygon:{"+ intersectsWKT +"}}} \
+        limit: 10 \
+        sortBy: { field: \"content.catalogProperties.startTimeUTC\", sortOrder: \"ASC\" } \
+        content: { catalogProperties: { \
+            startTimeUTC: { gt: \"" + startdate + "\" lt: \"" + enddate + "\" } \
+            vendor: { eq: \"ICEYE\" } \
+            passType:{eq: \"DESCENDING\"} \
+            antennaPointing:{eq: \"LEFT\"} \
+            acquisitionMode:{eq:\"ICE_SPOTLIGHT\"} \
+        } } ) { assignedId\n    uploadTime\n    header {\n      version\n      commonProperties {\n        sourceVendor\n        startTimeUTC\n        endTimeUTC\n      }\n    } \
+        content {\n      version\n      catalogProperties {\n        quicklookUrl\n        wktPolygon\n        geoJsonPolygon {\n          type\n          coordinates\n        }\n   vendor\n   startTimeUTC\n    endTimeUTC\n  passType\n        acquisitionMode\n        incidence {\n          incidenceNearDegs\n          incidenceFarDegs\n        }\n        collectionPlatform\n        antennaPointing\n }\n } \
+    }}"
+    
+    
+    paginationInfo = []
+    polygons = []
+    
     adx_response=adx.send_api_asset(
-        DataSetId="XXXXXXXXXXXXXXXXXXX",
-        RevisionId="XXXXXXXXXXXXXXXXXXX",
-        AssetId="XXXXXXXXXXXXXXXXXXX",
+        DataSetId="XXXXXXXXXXXXXXXXXXXX",
+        RevisionId="XXXXXXXXXXXXXXXXXXXX",
+        AssetId="XXXXXXXXXXXXXXXXXXXX",
         Method="POST",
         Path="/psdm/graphql",
         Body=body
@@ -20,21 +48,22 @@ def lambda_handler(event,context):
     adx_body = json.loads(adx_response["Body"])
     raw_data = adx_body['data']['ursa_udp_availablemetadatarecord']
     paginationInfo = adx_body['paginationInfo']
-    polygons = []
-    for polygon in raw_data:
-        coordinates = json.loads(polygon['content']['catalogProperties']['geoJsonPolygon']['coordinates'])
-        alt_coordinates = [[coordinate[1], coordinate[0]] for coordinate in coordinates[0]]
-        center = [(alt_coordinates[0][0] + alt_coordinates[2][0])/2, (alt_coordinates[0][1] + alt_coordinates[2][1])/2]
-        polygons.append({
-            "id": polygon['assignedId'],
-            "center": center,
-            "coordinates": alt_coordinates,
-            "startTimeUTC": polygon['content']['catalogProperties']['startTimeUTC'],
-            "quicklookUrl": polygon['content']['catalogProperties']['quicklookUrl'],
-            "passType": polygon['content']['catalogProperties']['passType'],
-            "antennaPointing": polygon['content']['catalogProperties']['antennaPointing']
-            
-        })
+    
+    if raw_data is not None:
+        for polygon in raw_data:
+            coordinates = json.loads(polygon['content']['catalogProperties']['geoJsonPolygon']['coordinates'])
+            alt_coordinates = [[coordinate[1], coordinate[0]] for coordinate in coordinates[0]]
+            center = [(alt_coordinates[0][0] + alt_coordinates[2][0])/2, (alt_coordinates[0][1] + alt_coordinates[2][1])/2]
+            polygons.append({
+                "id": polygon['assignedId'],
+                "center": center,
+                "coordinates": alt_coordinates,
+                "startTimeUTC": polygon['content']['catalogProperties']['startTimeUTC'],
+                "quicklookUrl": polygon['content']['catalogProperties']['quicklookUrl'],
+                "passType": polygon['content']['catalogProperties']['passType'],
+                "antennaPointing": polygon['content']['catalogProperties']['antennaPointing']
+                
+            })
     
     data = {
         "data": polygons,
